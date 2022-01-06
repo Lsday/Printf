38e58024fde5abc95275ba02c9d474444a2c148f
Olivier Ronda <oronda@c2r10p2.42nice.fr>
2022-01-06 14:18:33 +0100

    last modifs

--- ft_format_character.c
+++ ft_format_character.c
@@ -6,7 +6,7 @@
 /*   By: oronda <oronda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/24 18:49:41 by oronda            #+#    #+#             */
-/*   Updated: 2021/10/27 10:17:20 by oronda           ###   ########.fr       */
+/*   Updated: 2022/01/06 14:18:20 by oronda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int	ft_format_character(va_list lst)
 {
 	char	c;
 
+	
 	c = (char)(va_arg(lst, int));
 	ft_putchar_fd(c, 1);
 	return (1);
